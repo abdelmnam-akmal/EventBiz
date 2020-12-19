c75af567ca6b3b55e16cfa9a3e8ccfffd5a326e5
abdoakmal71 <abdoakmal71@gmail.com>
2020-12-19 19:54:37 +0200

    last Modified

--- project/js/main.js
+++ project/js/main.js
@@ -8,6 +8,17 @@ $(function () {
     $('.navbar-slide .links li').on('click', function () {
         $(this).addClass('active').siblings().removeClass('active');
     });
+    $('.btns-show li').on('click', function () {
+        $(this).addClass('active').siblings().removeClass('active');
+        $('#' + $(this).data('info')).addClass('active').siblings().removeClass('active');
+    });
+
+    // scroll to div on click
+    $('.header .icon-down i').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $('.about-event').offset().top
+        }, 1500);
+    });
 
     // navbar slider
     $('.navbar-slide').css('left', -$('.navbar-slide').innerWidth());
@@ -19,3 +30,21 @@ $(function () {
         }, 500);
     });
 })
+
+
+window.setInterval(function(){
+    var d = new Date();
+    var x = document.getElementById("hours");
+    var a = document.getElementById("minutes");
+    var v = document.getElementById("seconds");
+    var h = d.getHours();
+    var m = d.getMinutes();
+    var s = d.getSeconds();
+    x.innerHTML = h;
+    a.innerHTML = m;
+    v.innerHTML = s;
+  }, 1000);
+
+
+
+
